46f2893c4ab933e0a0f180d147c19a56c7d4592d
jaesoon3737 <wodud4019@gmail.com>
2023-12-27 15:08:43 +0900

    구글 서치 noindex 문제 해결 테스트중

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,10 @@ export const metadata: Metadata = {
   description: '프론트개발자 포트폴리오',
   keywords: ['Next.js', '프론트엔드', '프론트개발자' , '포트폴리오'],
   creator: 'Lee Jae Young',
+  robots: {
+    index: true,
+    follow: true
+  }
 }
 
 export default function RootLayout({
